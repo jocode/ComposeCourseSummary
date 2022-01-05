fbacf72eebba32f7dc4fc968ab4010cccb686dbe
Johan Mosquera <joncamilomoni@gmail.com>
2022-01-05 08:06:44 -0500

    Part 4. Creating an image card composable

--- ComposeCourse/app/src/main/java/com/example/composecourse/MainActivity.kt
+++ ComposeCourse/app/src/main/java/com/example/composecourse/MainActivity.kt
@@ -1,52 +1,90 @@
 package com.example.composecourse
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
+            val painter = painterResource(id = R.drawable.san_agustin)
+            val description = "This is a image"
+            val title = "Speak Now to interact with Assistant"
+
+            Box(
                 modifier = Modifier
-                    .background(Color.Red)
-                    .fillMaxHeight(0.5f)
-                    .fillMaxWidth()
-                    .border(5.dp, Color.Blue)
-                    .padding(5.dp)
-                    .border(5.dp, Color.Green)
-                    .padding(5.dp)
-                    .border(10.dp, Color.Gray)
-                    .padding(10.dp),
-                verticalArrangement = Arrangement.SpaceEvenly,
-                horizontalAlignment = Alignment.CenterHorizontally
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp)
             ) {
-                Text(text = "Hello", modifier = Modifier.clickable {
-                    Toast.makeText(this@MainActivity, "Hello", Toast.LENGTH_SHORT).show()
-                })
-                Spacer(modifier = Modifier.height(50.dp))
-                Text(text = "World")
+                ImageCard(painter = painter, contentDescription = description, title = title)
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp,
+        backgroundColor = Color.Black
+    ) {
+        Box(
+            modifier = Modifier.height(200.dp)
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
             }
         }
     }
