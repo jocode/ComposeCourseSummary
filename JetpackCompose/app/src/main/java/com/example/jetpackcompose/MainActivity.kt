a634350d08953275edf5b5a0503a81c7191d9ad7
Johan Mosquera <joncamilomoni@gmail.com>
2022-08-17 12:34:38 -0500

    Creating a textView -> Text in Compose

--- JetpackCompose/app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ JetpackCompose/app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -4,29 +4,45 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            helloApp()
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun helloApp() {
+    JetpackComposeTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            Greeting("Android")
+        }
+    }
+}
+
+@Composable
+fun exampleModifier() {
+    Text(
+        text = "Esto mola bien",
+        modifier = Modifier.padding(horizontal = 16.dp)
+    )
 }
 
 @Composable
