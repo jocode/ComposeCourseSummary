9ad3e678432a02a41a968022a0e68630d1722d9b
Johan Mosquera <joncamilomoni@gmail.com>
2022-08-18 17:28:26 -0500

    Column, Row and Scaffold on Android

--- JetpackCompose/README.md
+++ JetpackCompose/README.md
@@ -23,3 +23,91 @@ Se requiere la versión Artic Fox o superior para usar Compose.
 
 Los modificadores son como atributos extras que pueden recibir nuestras funciones, para definir en la vista.
 Como paddings, la longitud, elevación, etc.
+
+Algunos de los modificadores son:
+- `fillMaxSize()`
+- `background(Color.Blue)`
+- `fillMaxWidth()`
+- `fillMaxHeight()`
+
+
+## Columnas `Column(){}`
+
+Por defecto, las vistas se colocan una sobre otra. Por eso es necesario tener contenedores que nos permitan organizar la vistas en el layout que estemos definiendo. A través de:
+- Filas
+- Columnas
+- Cajas
+
+Los modificadores, siguen el orden de definición.
+Lo primero que se define, es lo primero que se aplica.
+Por ejemplo, si hacemos un elemento clickable antes, y luego definimos un padding, ese padding no será clickable.
+
+
+Los `LazyColumn` funcionan como un RecyclerView, a medida que hacemos scroll se cargan los nuevos componentes.
+Un LazyColumn, sólo puede contener **items**.
+
+```js
+LazyColumn(
+    modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Blue)
+) {
+    item {
+        Image(
+            painter = painterResource(id = R.drawable.img),
+            contentDescription = "Logo de la company",
+            Modifier.fillMaxWidth()
+        )
+        Text(
+            text = "jocode",
+            fontSize = 32.sp,
+            color = Color.White,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        Text(text = "Camilo", color = Color.White)
+        Text(text = "Mosquera", color = Color.White)
+    }
+}
+```
+
+
+## Filas `Rows(){}`
+
+Las filas nos permiten dibujar las vistas de forma horizontal, una tras otra.
+
+Al Igual que column, se tiene el  **`LazyRow`** que nos permite agregar scroll horizontal a los elementos.
+
+```js
+LazyRow(
+    horizontalArrangement = Arrangement.SpaceEvenly,
+    modifier = Modifier.fillMaxWidth().padding(16.dp)
+) {
+    item {
+        Text(text = "Kotlin", color = Color.White)
+        Text(text = "Java", color = Color.White)
+        Text(text = "JS", color = Color.White)
+        Text(text = "PHP", color = Color.White)
+        Text(text = "Python", color = Color.White)
+        Text(text = "C++", color = Color.White)
+    }
+}
+```
+
+## Estados (STATES)
+
+Las vistas se recrean, es decir cuando se modifica el valor, la vista de destruye y se recrea nuevamente.
+Los estados, serán los valores que se pondrán en nuestra vista que se mantendrán independientemente si se recrea la vista.
+
+Para almacenar estados en compose, podemos usar la palabra reservada **`remember`** para retener la informacion se las variables en compose y  pasarla por parámetro.
+
+Sin embargo, como al rotarse la pantalla se elimima toda la información, para recrearse nuevamente, podremos usar la función **rememberSaveable** para retener la información cuando se gira la pantalla.
+
+```js
+var counter by rememberSaveable { mutableStateOf(0) }
+```
+
+## Scaffold (TOOLBAR y FAB) 
+
+Este componente nos va a permitir simplificar mucho lo que tiene que ver con activity o fragment.
+Es como un molde donde podremos ir colocando los componentes.
--- JetpackCompose/app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ JetpackCompose/app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -1,29 +1,176 @@
 package com.example.jetpackcompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            helloApp()
+            JetpackComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    ViewContainer()
+                }
+            }
+
         }
     }
 }
 
 @Preview(showBackground = true)
+@Composable
+fun ViewContainer() {
+    Scaffold(
+        topBar = { Toolbar() },
+        content = {
+            Content()
+        },
+        floatingActionButton = { Fab() },
+        floatingActionButtonPosition = FabPosition.End
+    )
+}
+
+@Composable
+fun Toolbar() {
+    TopAppBar(
+        title = {
+            Text(
+                text = "jocode Profile",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        },
+        backgroundColor = Color.White,
+        elevation = 5.dp
+    )
+}
+
+@Composable
+fun Fab() {
+    val context = LocalContext.current
+    FloatingActionButton(onClick = {
+        Toast.makeText(context, "Hello world!", Toast.LENGTH_SHORT).show()
+    }) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_favorite),
+            contentDescription = "Favorite"
+        )
+    }
+}
+
+@Composable
+fun Content() {
+    var counter by rememberSaveable { mutableStateOf(0) }
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Blue)
+            .padding(16.dp)
+    ) {
+        item {
+            Image(
+                painter = painterResource(id = R.drawable.img),
+                contentDescription = "Logo de la company",
+                Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+
+            Row(
+                modifier = Modifier.padding(top = 8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_favorite),
+                    contentDescription = "Like",
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .clickable {
+                            counter++
+                        }
+                )
+                Text(
+                    text = counter.toString(),
+                    color = Color.White,
+                    modifier = Modifier.padding(start = 4.dp)
+                )
+            }
+
+            Text(
+                text = "jocode",
+                fontSize = 32.sp,
+                color = Color.White,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+            LazyRow(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                item {
+                    Tag("Android")
+                    Tag("Java")
+                    Tag("Kotlin")
+                    Tag("Javascript")
+                    Tag("Python")
+                    Tag("PHP")
+                    Tag("Go")
+                    Tag("C++")
+                    Tag("HTML")
+                    Tag("CSS")
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Tag(text: String = "Hola mundo") {
+    Text(
+        text = text,
+        color = Color.White,
+        modifier = Modifier
+            .padding(horizontal = 5.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(color = Color.Gray)
+            .padding(horizontal = 10.dp, vertical = 5.dp)
+    )
+}
+
+//@Preview(showBackground = true)
 @Composable
 fun helloApp() {
     JetpackComposeTheme {
@@ -50,7 +197,7 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeTheme {
